85a85ac1d5754feaa867cad696f8530fa120ee4e
sandeepkumar1209 <sandeepkumarpanda487@gmail.com>
2021-02-02 23:00:52 +0530

    Adding Object class method toString to class B for meaningful rpresentation

--- B.java
+++ B.java
@@ -2,6 +2,10 @@ class B
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Inside class B");
+		public toString(){
+			return " B";
+		}
+		B b= new B();
+		System.out.println(b);
 	}
 }
